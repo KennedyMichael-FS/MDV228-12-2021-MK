388a2e894df3eded994da823d56066172f580bb8
KennedyMichael-FS <mskennedy@student.fullsail.edu>
2021-12-19 07:06:23 -0500

    Commit Visual Update 01
    Updated some visuals for hi-fi version
    
    Includes background imagery
    Includes generic part names instead of sequential 'item' listings

---
+++ Beta Dev - Visual Update/SwapPartDEV/ItemPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SwapPartDEV
+{
+    public partial class ItemPage : ContentPage
+    {
+        public ItemPage()
+        {
+            BackgroundImageSource = "https://i.ibb.co/61pDNkf/bgimg.jpg";
+
+            InitializeComponent();
+            backButton.Pressed += BackButton_Pressed;
+
+        }
+
+        private void BackButton_Pressed(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ Beta Dev - Visual Update/SwapPartDEV/ItemsView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+
+namespace SwapPartDEV
+{
+    public partial class ItemsView : ContentPage
+    {
+        public static int CategoryNum;
+        public ObservableCollection<Items> AllItems;
+
+        public ItemsView(int categorynum)
+        {
+
+            CategoryNum = categorynum;
+            BackgroundImageSource = "https://i.ibb.co/61pDNkf/bgimg.jpg";
+
+            InitializeComponent();
+            this.AllItems = new ObservableCollection<Items>(ItemData.Get());
+            collectionView.SelectionMode = SelectionMode.Single;
+
+            collectionView.SelectionChanged += CollectionView_SelectionChanged;
+            backButton.Pressed += BackButton_Pressed;
+
+        }
+
+        private void BackButton_Pressed(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            collectionView.ItemsSource = AllItems;
+        }
+
+        private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+
+            Navigation.PushModalAsync(new ItemPage());
+        }
+    }
+}
